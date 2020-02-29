3b2033ca626a5f0adc6b67bac5c3e9fbdf8972b2
Thinh Hua <hquangthinh@gmail.com>
2020-02-29 10:49:33 +0700

    add example plugin

---
+++ Plugins/Grand.Plugin.Misc.ExamplePlugin/AdminTabsExample.cs
@@ -0,0 +1,30 @@
+﻿using Grand.Framework.Events;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Grand.Plugin.Misc.ExamplePlugin
+{
+    public class AdminTabsExample : INotificationHandler<AdminTabStripCreated>
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AdminTabsExample(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public Task Handle(AdminTabStripCreated eventMessage, CancellationToken cancellationToken)
+        {
+            if (eventMessage.TabStripName == "category-edit")
+            {
+                var categoryId = Convert.ToString(_httpContextAccessor.HttpContext.GetRouteValue("ID"));
+                eventMessage.BlocksToRender.Add(("test new tab", new Microsoft.AspNetCore.Html.HtmlString($"<div>TEST {categoryId}</div>")));
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Plugins/Grand.Plugin.Misc.ExamplePlugin/Services/OverrideProductService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grand.Core;
+using Grand.Core.Caching;
+using Grand.Core.Data;
+using Grand.Core.Domain.Catalog;
+using Grand.Core.Domain.Common;
+using Grand.Core.Domain.Customers;
+using Grand.Core.Domain.Localization;
+using Grand.Core.Domain.Seo;
+using Grand.Services.Catalog;
+using Grand.Services.Messages;
+using MediatR;
+
+namespace Grand.Plugin.Misc.ExamplePlugin.Services
+{
+    public class OverrideProductService: ProductService
+    {
+        public OverrideProductService(ICacheManager cacheManager, IRepository<Product> productRepository,
+            IRepository<ProductReview> productReviewRepository, IRepository<UrlRecord> urlRecordRepository,
+            IRepository<Customer> customerRepository, IRepository<CustomerRoleProduct> customerRoleProductRepository,
+            IRepository<CustomerTagProduct> customerTagProductRepository,
+            IRepository<ProductDeleted> productDeletedRepository,
+            IRepository<CustomerProduct> customerProductRepository, IProductAttributeService productAttributeService,
+            IProductAttributeParser productAttributeParser,
+            ISpecificationAttributeService specificationAttributeService,
+            IWorkflowMessageService workflowMessageService, IWorkContext workContext,
+            LocalizationSettings localizationSettings, CommonSettings commonSettings, CatalogSettings catalogSettings,
+            IMediator mediator, IRepository<ProductTag> productTagRepository, IServiceProvider serviceProvider) : base(
+            cacheManager, productRepository, productReviewRepository, urlRecordRepository, customerRepository,
+            customerRoleProductRepository, customerTagProductRepository, productDeletedRepository,
+            customerProductRepository, productAttributeService, productAttributeParser, specificationAttributeService,
+            workflowMessageService, workContext, localizationSettings, commonSettings, catalogSettings, mediator,
+            productTagRepository, serviceProvider)
+        {
+        }
+
+        public override Task DeleteProduct(Product product)
+        {
+            throw new Exception("[OverrideProductService]: You cannot delete product");
+        }
+    }
+}

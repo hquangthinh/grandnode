d7730d7bd18a712b450f7bc6148c4da7296384a0
Thinh Hua <hquangthinh@gmail.com>
2020-04-05 13:06:46 +0700

    api for basi app and front store setup

---
+++ Plugins/Grand.Plugin.Api.Extended/Controllers/MobileCatalogApiController.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using Grand.Services.Catalog;
+using Grand.Web.Areas.Api.Controllers;
+using Grand.Web.Interfaces;
+using Grand.Web.Models.Catalog;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grand.Plugin.Api.Extended.Controllers
+{
+    [Route("api/mobile-catalog")]
+    public class MobileCatalogApiController : BaseApiController
+    {
+        #region Fields
+
+        private readonly ICatalogViewModelService _catalogViewModelService;
+
+        #endregion
+
+        #region Constructors
+
+        public MobileCatalogApiController(ICatalogViewModelService catalogViewModelService)
+        {
+            _catalogViewModelService = catalogViewModelService;
+        }
+
+        #endregion
+
+        #region Categories
+
+        [Route("categories/{categoryId}")]
+        [HttpPost]
+        public virtual async Task<IActionResult> Category([FromRoute] string categoryId, [FromBody] CatalogPagingFilteringModel command)
+        {
+            var category = await _catalogViewModelService.GetCategoryById(categoryId);
+
+            if (category == null)
+                return Ok(new CategoryModel());
+
+            var viewModel = await _catalogViewModelService.PrepareCategory(category, command);
+
+            return Ok(viewModel);
+        }
+
+        #endregion
+    }
+}
--- Plugins/Grand.Plugin.Api.Extended/Controllers/MobileProductApiController.cs
+++ Plugins/Grand.Plugin.Api.Extended/Controllers/MobileProductApiController.cs
@@ -1,12 +1,14 @@
 ﻿using System.Threading.Tasks;
 using Grand.Services.Catalog;
+using Grand.Web.Areas.Api.Controllers;
 using Grand.Web.Interfaces;
+using Grand.Web.Models.Catalog;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Grand.Plugin.Api.Extended.Controllers
 {
     [Route("api/mobile-product")]
-    public class MobileProductApiController : BaseAuthorizedApiController
+    public class MobileProductApiController : BaseApiController
     {
         #region Fields
         
@@ -40,7 +42,22 @@ namespace Grand.Plugin.Api.Extended.Controllers
 
             return Ok(model);
         }
-        
+
+        [Route("{productId}/reviews")]
+        [ProducesResponseType(200, Type = typeof(ProductReviewsModel))]
+        public virtual async Task<IActionResult> GetReviewsForProduct([FromRoute] string productId, 
+            [FromQuery] int page, [FromQuery]  int pageSize)
+        {
+            var model = new ProductReviewsModel();
+            var product = await _productService.GetProductById(productId);
+            if (product == null || !product.Published || !product.AllowCustomerReviews)
+                return Ok(model);
+
+            await _productViewModelService.PrepareProductReviewsModel(model, product, pageSize);
+
+            return Ok(model);
+        }
+
         #endregion
     }
 }

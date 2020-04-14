6d3a607f465530b5ae12c83c7d2eca7da415be9c
Dan Van <vandanvd@gmail.com>
2020-04-14 15:43:27 +0700

    load mobile customer info

--- Plugins/Grand.Plugin.Api.Extended/Controllers/MobileCustomerApiController.cs
+++ Plugins/Grand.Plugin.Api.Extended/Controllers/MobileCustomerApiController.cs
@@ -1,7 +1,37 @@
-﻿namespace Grand.Plugin.Api.Extended.Controllers
+﻿using Grand.Core;
+using Grand.Core.Domain.Customers;
+using Grand.Web.Areas.Api.Controllers;
+using Grand.Web.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Grand.Plugin.Api.Extended.Controllers
 {
-    public class MobileCustomerApiController : BaseAuthorizedApiController
-    {
-        
-    }
+	[Route("api/mobile-customer")]
+	public class MobileCustomerApiController: BaseApiController
+	{
+		private readonly ICustomerViewModelService _customerViewModelService;
+		IWorkContext _workContext;
+
+		#region Constructor
+		public MobileCustomerApiController(ICustomerViewModelService customerViewModelService,
+			IWorkContext workContext)
+		{
+			_customerViewModelService = customerViewModelService;
+			_workContext = workContext;
+
+		}
+		#endregion
+
+		[Route("")]
+		[HttpGet]
+		public async Task<IActionResult> GetCurrentCustomerInfo()
+		{
+			return !_workContext.CurrentCustomer.IsRegistered() ? 
+				Unauthorized(HttpStatusCode.Unauthorized) : 
+				(IActionResult)Ok(_workContext.CurrentCustomer);
+		}
+
+	}
 }
